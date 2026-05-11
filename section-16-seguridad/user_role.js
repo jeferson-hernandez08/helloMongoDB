98bbf85b82126c1aaf3ec67e0386795a8f801421
Jeferson <jefer.hernandez1@gmail.com>
2026-05-11 16:45:18 -0500

    Seccion 16: Seguridad Finalizado

---
+++ section-16-seguridad/user_role.js
@@ -0,0 +1,33 @@
+db.createUser(
+    {
+        user: "userDataScience",
+        pwd: passwordPrompt(),
+        roles: [
+            {
+                role: "read",
+                db: "onlineEducation"
+            },
+            {
+                role: "read",
+                db: "Datademy"
+            }
+        ]
+    }
+)
+
+
+db.updateUser(
+    "userDataScience",
+    {
+        roles: [
+            {
+                role: "readWrite",
+                db: "onlineEducation"
+            },
+            {
+                role: "readWrite",
+                db: "Datademy"
+            }
+        ]
+    }
+)

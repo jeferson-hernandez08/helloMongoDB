dabdfe896f127e68f86797219d56d8cdcb0cb0c8
Jeferson <jefer.hernandez1@gmail.com>
2026-05-14 09:31:44 -0500

    app: Conexión, estructura con clase e import

--- appRegistroProducto/db.py
+++ appRegistroProducto/db.py
@@ -2,15 +2,15 @@ from pymongo import MongoClient
 import certifi
 import urllib
 
-MONGO_URI = "mongodb+srv://adminDatademy:" + urllib.parse.quote('P@ssw0rd1') + "@clusterdatademy.qduii2a.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://jeferson_hernandez_udemy:" + urllib.parse.quote('ASDasd.123') + "@cluster0.resnduv.mongodb.net/?appName=Cluster0"
 ca = certifi.where()
 
 def dbConnection():
     try:
         client = MongoClient(MONGO_URI, tlsCAFile = ca)
-        db = client["db_app_product"]
+        db = client["db_app_product"]    # Nombre de base de datos db_app_product, le podemos cualquier nombre aquí
     except ConnectionError as e:
         print("Error de conexión con la base de datos!")
     else:
-        print("Estás conectado!")
+        print("Estás conectado!")   
         return db
--- appRegistroProducto/product.py
+++ appRegistroProducto/product.py
@@ -1,12 +1,12 @@
 class Product:
-    def __init__(self, nombre, precio, cantidad):
-        self.nombre = nombre
+    def __init__(self, nombre, precio, cantidad):   # Constructor
+        self.nombre = nombre      # Atributos
         self.precio = precio
         self.cantidad = cantidad
 
-    def toDBCollection(self):
+    def toDBCollection(self):      # Función para darnos la estrucutura para poder almacenar los daatos en la DB, retorna un objeto con nuestros datos 
         return{
-            'nombre': str(self.nombre),
+            'nombre': str(self.nombre),   # Esto es el documento, clave: valor
             'precio': float(self.precio),
             'cantidad': int(self.cantidad)
         }
